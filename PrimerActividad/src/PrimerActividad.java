e3e198d43d42379e7c0b7138ec0e4d9998f2a6fb
LAPTOP-3AE5OCH1\iks80 <iks8001@gmail.com>
2021-04-04 16:18:06 -0300

    Add "Ivan"

--- PrimerActividad/src/PrimerActividad.java
+++ PrimerActividad/src/PrimerActividad.java
@@ -1,6 +1,6 @@
 public class PrimerActividad 
 {
 	public static void main(String args[]) {
-		System.out.println("Laureano" + "Eliana");
+		System.out.println("Laureano" + "Eliana" + "Ivan");
 	}
 }
