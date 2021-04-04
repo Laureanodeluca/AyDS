7caf4d46408a6c5fa54d2f415addbc6ac9e0d520
eliko <elikohon@gmail.com>
2021-04-04 20:39:27 -0300

    Update PrimerActividad.java
    Add Eliana

--- PrimerActividad/src/PrimerActividad.java
+++ PrimerActividad/src/PrimerActividad.java
@@ -1,6 +1,6 @@
 public class PrimerActividad 
 {
 	public static void main(String args[]) {
-		System.out.println("Laureano" + "Eliana" + "Ivan");
+		System.out.println("Laureano"  + "Ivan" + "Eliana");
 	}
 }
