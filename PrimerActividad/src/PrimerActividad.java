16a9c5846aac7dd54aef99773ea71f6d151ace65
FaustoFe <51035010+FaustoFe@users.noreply.github.com>
2021-04-04 22:10:10 -0300

    Update PrimerActividad.java

--- PrimerActividad/src/PrimerActividad.java
+++ PrimerActividad/src/PrimerActividad.java
@@ -1,6 +1,6 @@
-public class PrimerActividad 
+public class PrimerActividad
 {
 	public static void main(String args[]) {
-		System.out.println("Laureano"  + "Ivan" + "Eliana");
+		System.out.println("Laureano" + "Ivan" + "Eliana" + "Fausto");
 	}
 }
